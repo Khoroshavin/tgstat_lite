0cf11897c5fbfea9a14a853977614068318ef0b0
Khoroshavin <khoroshavin.russia@gmail.com>
2021-03-19 00:29:06 +0100

    change color mode

--- js/main.js
+++ js/main.js
@@ -1,16 +1,53 @@
+const checkTheme = () => {
+  const styleMode = document.querySelector('#mode');
+  let currTheme = localStorage.getItem('mode');
+  if (currTheme === null) {
+    localStorage.setItem('mode', 'light');
+  }
+  else if(currTheme === 'light') {
+    styleMode.setAttribute('href', 'css/light_theme.css')
+  }
+  else if(currTheme === 'dark') {
+    styleMode.setAttribute('href', 'css/dark_theme.css')
+  }
+};
+
+checkTheme();
+
+
+
 document.addEventListener('DOMContentLoaded', (event) => {
-  
+
+  // change theme
+  const changeTheme = () => {
+    const styleMode = document.querySelector('#mode');
+    const changeThemeBtn = document.querySelector('.theme__btn');
+    let currMode = localStorage.getItem('mode');
+    changeThemeBtn.addEventListener('click', () => {
+      if (currMode == 'light') {
+        styleMode.setAttribute('href', 'css/dark_theme.css');
+        currMode = 'dark';
+        localStorage.setItem('mode', 'dark');
+      } else {
+        styleMode.setAttribute('href', 'css/light_theme.css');
+        currMode = 'light';
+        localStorage.setItem('mode', 'light');
+      }
+    });
+
+  };
+
   // 1 tab top chanels sort
-  const sortTopChanels = () =>{
+  const sortTopChanels = () => {
     const topChanelsPopSortBtn = document.querySelector('.top__chanels_pop_sort');
     const topChanelsAbcSortBtn = document.querySelector('.top__chanels_abc_sort');
-  
+
     topChanelsPopSortBtn.addEventListener('click', () => {
-  
+
     });
-  
+
     topChanelsAbcSortBtn.addEventListener('click', () => {
-      
+
     });
   };
 
@@ -18,13 +55,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const sortTopChats = () => {
     const topChatsPopSortBtn = document.querySelector('.top__chats_pop_sort');
     const topChatsAbcSortBtn = document.querySelector('.top__chats_abc_sort');
-  
+
     topChatsPopSortBtn.addEventListener('click', () => {
-  
+
     });
-  
+
     topChatsAbcSortBtn.addEventListener('click', () => {
-      
+
     });
   };
 
@@ -32,18 +69,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const showHideCats = () => {
     const chanelsCategoryOpen = document.querySelector('.chanels__category_open');
     const chanelsCategory = document.querySelector('.chanels__category');
-    
+    const chanelsCategoryClose = document.querySelector('.chanels__category_close');
     // show cats
     chanelsCategoryOpen.addEventListener('click', () => {
       chanelsCategory.classList.add("chanels__category_opened");
       chanelsCategoryOpen.style.display = "none";
+      chanelsCategoryClose.style.display = "initial";
     });
-  
+
     // hide cats
-    const chanelsCategoryClose = document.querySelector('.chanels__category_close');
     chanelsCategoryClose.addEventListener('click', () => {
       chanelsCategory.classList.remove("chanels__category_opened");
       chanelsCategoryOpen.style.display = "block";
+      chanelsCategoryClose.style.display = "none";
     })
   };
 
@@ -74,13 +112,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const tabChanelsSort = () => {
     const chanelsPopSortBtn = document.querySelector('.chanels_pop_sort');
     const chanelsAbcSortBtn = document.querySelector('.chanels_abc_sort');
-  
+
     chanelsPopSortBtn.addEventListener('click', () => {
-  
+
     });
-  
+
     chanelsAbcSortBtn.addEventListener('click', () => {
-      
+
     });
   };
 
@@ -88,14 +126,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
 
-
-
-
-  
-
-  sortTopChanels();// 1 tab top chanels sort
-  sortTopChats();// 1 tab top chats sort
-  showHideCats();//2 tab show/hide of categories
-  choiceChanelsCategory();// 2 tab choice chanels category
-  tabChanelsSort();// 2 tab chanels sort
+  changeTheme();
+  sortTopChanels(); // 1 tab top chanels sort
+  sortTopChats(); // 1 tab top chats sort
+  showHideCats(); //2 tab show/hide of categories
+  choiceChanelsCategory(); // 2 tab choice chanels category
+  tabChanelsSort(); // 2 tab chanels sort
 });
